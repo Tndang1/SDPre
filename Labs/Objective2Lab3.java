6534a763c562c5658cc6ab23c709bd8257a0d36d
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-06 17:40:44 -0500

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 1;
+    num2 = 7;
+    num3 = 124;
+    average = ((num1 + num2 + num3) / 3);
+
+    System.out.println(average);
+  }
+}
