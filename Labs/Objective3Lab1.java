af4de6c32c8930ef2c37f7a6cb13342f3bdd1d28
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-06 18:10:13 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 1;
+    num2 = 23;
+    num3 = 67;
+    average = ((num1 + num2 + num3) / 3);
+    System.out.println(average);
+  }
+}
