5869895c04e9d9817b19c275c58e8d632a140706
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-06 17:24:25 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Thomas Dang";
+    int age = 29;
+    String state = "Kansas";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
