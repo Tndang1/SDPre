2f752f8dab99adc520fada7fd5b254b620c87d68
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-13 11:13:32 -0500

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args){
+    int sum = 0;
+    int counter = 0;
+    while(counter <=20){
+      sum = sum + counter;
+      counter ++;
+    }
+    System.out.println(sum);
+  }
+}
