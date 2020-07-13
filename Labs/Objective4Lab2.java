9102c87214be9e1f13623214e35c6c90b512a06c
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-13 06:45:32 -0500

    complete objective4lab2

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -5,21 +5,21 @@ public class Objective4Lab2 {
 
       Scanner keyboard = new Scanner(System.in);
 
-      //int num1 = 0, num2 = 0, num3 = 0;
-      //double dub1 = 0.0, dub2 = 0.0, dub3 = 0.0;
+      int num1 = 0, num2 = 0, num3 = 0;
+      double dub1 = 0.0, dub2 = 0.0, dub3 = 0.0;
 
       System.out.println("Please enter the first whole number you would like to add.");
-      int num1 = keyboard.nextInt();
+      num1 = keyboard.nextInt();
       System.out.println("Please enter the first second number you would like to add.");
-      int num2 = keyboard.nextInt();
+      num2 = keyboard.nextInt();
       System.out.println("Please enter the first third number you would like to add.");
-      int num3 = keyboard.nextInt();
+      num3 = keyboard.nextInt();
       System.out.println("Please enter the first decimal number you would like to add.");
-      double dub1 = keyboard.nextDouble();
+      dub1 = keyboard.nextDouble();
       System.out.println("Please enter the first decimal number you would like to add.");
-      double dub2 = keyboard.nextDouble();
+      dub2 = keyboard.nextDouble();
       System.out.println("Please enter the first decimal number you would like to add.");
-      double dub3 = keyboard.nextDouble();
+      dub3 = keyboard.nextDouble();
 
       int sum = num1 + num2 + num3;
       double dubSum = dub1 + dub2 + dub3;
