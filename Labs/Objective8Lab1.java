5d56833cd6ce2d2feac90ced7dd6ba00932ad7f2
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-07-13 15:57:05 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int up=1; up <= 10; up++){
+      System.out.println(up);
+    }
+    for (int down=10; down > 0; down --){
+      System.out.println(down);
+    }
+  }
+}
